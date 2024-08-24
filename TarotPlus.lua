96c90273ab9a4c1d5d5c02eba4f019325a3799db
Max Ostler <maxcostler@gmail.com>
2024-08-24 13:12:33 -0600

    Added Mirror Tarot

--- TarotPlus.lua
+++ TarotPlus.lua
@@ -11,6 +11,26 @@
 local mod_path = '' .. SMODS.current_mod.path
 SMODS.Atlas { key = 'Consumables', path = 'tarot.png', px = 71, py = 95 }
 
+function enhanceToCenter(enhance_str)
+    local centers = {
+    ["Bonus"] = G.P_CENTERS.m_bonus,
+    ["Mult"] = G.P_CENTERS.m_mult,
+    ["Wild Card"] = G.P_CENTERS.m_wild,
+    ["Glass Card"] = G.P_CENTERS.m_glass,
+    ["Steel Card"] = G.P_CENTERS.m_steel,
+    ["Stone Card"] = G.P_CENTERS.m_stone,
+    ["Gold Card"] = G.P_CENTERS.m_gold,
+    ["Lucky Card"] = G.P_CENTERS.m_lucky,
+    }
+
+    for k, v in pairs(centers) do
+        if k == enhance_str.ability.name then
+            return v
+        end
+    end
+    return false
+end
+
 -- Fire Tarot
 SMODS.Consumable {
     atlas = 'Consumables',
@@ -251,6 +271,65 @@ SMODS.Consumable {
         delay(0.6)
     end
 }
+
+-- Mirror Tarot
+SMODS.Consumable {
+    atlas = 'Consumables',
+    key = 'mirror',
+    set = 'Tarot',
+    discovered = true,
+    pos = { x = 0, y = 0 },
+    config = {extra = 2},
+    loc_vars = function(self, card)
+    end,
+    loc_txt = {
+        ['en-us'] = {
+            name = 'The Mirror',
+            text = {"Select {C:attention}2{} cards.",
+                    "Left card gains right",
+                    "card modifications."
+                    }
+        },
+    },
+    can_use = function(self, card)
+        return #G.hand.highlighted == 2
+    end,
+
+    use = function(self, card, area, copier)
+        -- Flip Cards
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.4, func = function()
+            play_sound('tarot1')
+            card:juice_up(0.3, 0.5)
+            return true end }))
+        for i=1, #G.hand.highlighted do
+            local percent = 1.15 - (i-0.999)/(#G.hand.highlighted-0.998)*0.3
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('card1', percent);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
+        end
+        delay(0.2)
+
+        -- Transfer Card Modifications
+        G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.1, func = function()
+            if G.hand.highlighted[2].seal then
+            G.hand.highlighted[1]:set_seal(G.hand.highlighted[2].seal, true, true)
+            end
+            if G.hand.highlighted[2].edition then
+            G.hand.highlighted[1]:set_edition(G.hand.highlighted[2].edition, true, true)
+            end
+            if enhanceToCenter(G.hand.highlighted[2]) then
+               G.hand.highlighted[1]:set_ability(enhanceToCenter(G.hand.highlighted[2]), nil, false)
+            end
+            return true end }))
+
+        -- Unflip Cards
+        for i=1, #G.hand.highlighted do
+            local percent = 0.85 + (i-0.999)/(#G.hand.highlighted-0.998)*0.3
+            G.E_MANAGER:add_event(Event({trigger = 'after',delay = 0.15,func = function() G.hand.highlighted[i]:flip();play_sound('tarot2', percent, 0.6);G.hand.highlighted[i]:juice_up(0.3, 0.3);return true end }))
+        end
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.2,func = function() G.hand:unhighlight_all(); return true end }))
+        delay(0.5)
+    end
+}
+
 
 ----------------------------------------------
 ------------MOD CODE END----------------------
